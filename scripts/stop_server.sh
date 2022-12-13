8e2dedade85abfca06b974fe66a1c96a72c3d4ce
anil samal <rajuanil22@gmail.com>
2022-12-13 12:13:31 +0530

    appspecyml changed

--- scripts/stop_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# this file runs first, so during an initail installation error might happen
-
-# simply stop the application process using pm2
-echo stopping server
-sudo su
-pm2 stop npm
-exit
